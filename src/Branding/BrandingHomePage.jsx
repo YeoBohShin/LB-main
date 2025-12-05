bbfa799cbaca7093a49cfa33287203aadd8e86f4
Boh Shin Yeo <e0968907@u.nus.edu>
2025-12-05 14:44:49 +0800

    Fix homepage sizing

--- src/Branding/BrandingHomePage.jsx
+++ src/Branding/BrandingHomePage.jsx
@@ -5,22 +5,22 @@ export default function BrandingHomePage() {
   return (
     <div className="flex flex-col xl:flex-row-reverse items-center xl:min-h-screen mb-5 xl:mb-0 mx-5 xl:mx-0">
       <motion.div
-        className="flex flex-col items-center justify-center text-center p-8 xl:absolute xl:top-24/5 xl:left-1/7 xl:w-1/4 bg-base-200 rounded-4xl shadow-lg z-10 mb-5 xl:mb-0"
+        className="flex flex-col items-center justify-center text-center p-10 w-full xl:absolute xl:top-24/5 xl:left-1/7 xl:w-1/4 bg-base-200 rounded-4xl shadow-lg z-10 mb-5 xl:mb-0"
         initial={{ x: 100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true, amount: 0.3 }}
         transition={{ type: "spring", stiffness: 50, damping: 20, duration: 0.8 }}
         >
         <h1 className="text-4xl font-bold mb-4">Branding</h1>
-        <p className="mb-3">
+        <p className="mb-5">
           Want your logo rolling around town? Here at Limo Butler Pte Ltd, we can make it happen.
         </p>
         <Link to="/branding" className="btn btn-primary">Find out more</Link>
       </motion.div>
       <motion.img
-        src="./PanPacific.jpg"
+        src="./Branding.jpeg"
         alt="Branding"
-        className="w-full rounded-4xl max-w-4xl xl:m-10"
+        className="w-full rounded-4xl xl:max-w-4xl xl:m-10"
         initial={{ x: -100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true, amount: 0.3 }}
--- src/Services/ServicesHomePage.jsx
+++ src/Services/ServicesHomePage.jsx
@@ -5,22 +5,22 @@ export default function ServicesHomePage() {
   return (
     <div className="flex flex-col xl:flex-row xl:min-h-screen text-center mx-5 items-center justify-start text-primary mb-5 xl:mb-0">
       <motion.div
-        className="bg-base-200 z-10 p-10 rounded-4xl shadow-lg relative flex flex-col justify-center items-center mt-5 xl:mt-0 xl:absolute xl:top-19/5 xl:right-1/7 max-w-md"
+        className="bg-base-200 z-10 p-10 rounded-4xl w-full shadow-lg relative flex flex-col justify-center items-center mt-5 xl:mt-0 xl:absolute xl:top-19/5 xl:right-1/7 xl:w-1/4"
         initial={{ x: 100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true, amount: 0.3 }}
         transition={{ type: "spring", stiffness: 50, damping: 20, duration: 0.8 }}
       >
         <h1 className="text-4xl mb-5">Our Services</h1>
-        <p className="text-lg mb-10">
+        <p className="text-lg mb-5">
           We offer a range of premium limousine services tailored to your needs.
         </p>
-        <Link to="/services" className="btn btn-primary mb-5">
-          View All Services
+        <Link to="/services" className="btn btn-primary">
+          View Services
         </Link>
       </motion.div>
       <motion.div
-        className="w-full max-w-4xl mt-5 xl:mt-0"
+        className="w-full xl:max-w-4xl mt-5 xl:mt-0"
         initial={{ x: -100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true, amount: 0.3 }}
