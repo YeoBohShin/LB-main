6134f3cc7ee223a8bfdb71ec77baf81e9170ba8a
Boh Shin Yeo <e0968907@u.nus.edu>
2025-12-05 16:07:40 +0800

    Update Footer font size

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,11 +3,11 @@ import LBLogoFooter from "./Logo/LBLogoFooter";
 export default function Footer() {
     return (
         <footer className="bg-base-200 text-base-content p-4 w-full flex flex-col">
-          <div className="flex flex-col justify-between xl:flex-row text-left">
+          <div className="flex flex-col justify-between md:flex-row text-left">
             <LBLogoFooter />
           <ul className="flex flex-col mt-5">
-            <h1 className="text-3xl font-bold text-left ml-5">Contact Us</h1>
-            <li className="text-lg m-5 mb-0 flex flex-row">
+            <h1 className="text-2xl font-bold text-left ml-5">Contact Us</h1>
+            <li className="text-md m-5 mb-0 flex flex-row">
               <strong className="mr-3">Phone: </strong>
               <div className="flex flex-col">
                 <p>
@@ -15,7 +15,7 @@ export default function Footer() {
                 </p>
               </div>
             </li>
-            <li className="text-lg m-5 mb-5 flex flex-row">
+            <li className="text-md m-5 mb-5 flex flex-row">
               <strong className="mr-3">Email: </strong>
               <a href="mailto:enquiries@limobutler.com.sg" className="text-blue-500 hover:underline">
                 enquiries@limobutler.com.sg
@@ -23,24 +23,13 @@ export default function Footer() {
             </li>
           </ul>
           <div className="flex flex-col mt-5">
-            <h1 className="text-3xl font-bold ml-5">Find Us</h1>
-          <div className="text-lg m-5 mb-5 flex flex-row xl:mb-0">
+            <h1 className="text-2xl font-bold ml-5">Find Us</h1>
+          <div className="text-md m-5 mb-5 flex flex-row xl:mb-0">
             <strong className="mr-3">Address: </strong>
             <p>
               33 Ubi Avenue 3, VERTEX, #05-67, Singapore 408868
             </p>
           </div>
-          {/* <div className="text-lg m-5 mb-10 flex flex-row">
-            <strong className="mr-3">Operating Hours: </strong>
-            <div className="flex flex-col text-left">
-              <p>
-                Monday to Friday: 9:00 AM - 6:00 PM
-              </p>
-              <p>
-                Saturday, Sunday & Public Holidays: Closed
-              </p>
-            </div>
-          </div> */}
         </div>
         </div>
         <div>
