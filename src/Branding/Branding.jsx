169986fa31c1896161c5cfc45f30b30b4b45c9ff
Boh Shin Yeo <e0968907@u.nus.edu>
2025-12-05 17:11:40 +0800

    Add image to branding

--- src/Branding/Branding.jsx
+++ src/Branding/Branding.jsx
@@ -14,20 +14,32 @@ export default function Branding() {
         className="h-auto"
       />
       <h1 className="hidden text-7xl font-bold xl:absolute xl:block xl:top-7/9">Branding</h1>
-      <div className="flex flex-row-reverse items-center justify-center xl:justify-start flex-wrap xl:flex-nowrap xl:m-5 gap-10">
-        <div className="text-center m-10 xl:my-0">
-          <h1 className="text-4xl font-semibold text-center mb-5">Mark your Brand</h1>
-          <p className="text-lg max-w-3xl leading-relaxed text-left">
+      <div className="flex flex-row-reverse items-center justify-center xl:justify-start flex-wrap xl:flex-nowrap xl:m-5 xl:gap-10">
+        <img
+          src="./BrandingRightCar.jpeg"
+          alt="Limo Branding"
+          className="h-auto w-full xl:max-w-lg xl:rounded-4xl xl:shadow-lg hidden xl:block"
+        />
+        <div className="text-center m-10 xl:m-0">
+          <h1 className="text-4xl font-semibold text-center my-5">Mark your Brand</h1>
+          <p className="text-lg max-w-3xl text-center">
             We blend sophistication with service, offering premium transport experiences 
-            that go beyond getting from point to point. 
+            that go beyond getting from point to point. Elevate your brand's visibility with our bespoke limousine branding solutions.
+          </p>
+          <p className="text-lg max-w-3xl text-center">
             Share your design with us, we'll handle the print and installation. 
             Whether it's one car or a full fleet, we'll ensure your brand stands out on the road.
           </p>
-          </div>
-          <img
+        </div>
+        <img
           src="./LeftCar.jpg"
           alt="Limo Branding"
-          className="h-auto w-full xl:max-w-2xl xl:rounded-4xl xl:shadow-lg"
+          className="h-auto w-full xl:max-w-lg xl:rounded-4xl xl:shadow-lg"
+        />
+        <img
+          src="./BrandingRightCar.jpeg"
+          alt="Limo Branding"
+          className="h-auto w-full xl:max-w-lg xl:rounded-4xl xl:shadow-lg xl:hidden"
         />
       </div>
       <Footer />
--- src/Branding/BrandingHomePage.jsx
+++ src/Branding/BrandingHomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function BrandingHomePage() {
   return (
-    <div className="flex flex-col xl:flex-row-reverse items-center xl:min-h-screen mb-5 xl:mb-0 mx-5 xl:mx-0">
+    <div className="flex flex-col xl:flex-row-reverse items-center xl:min-h-screen xl:mb-0 mx-5 xl:mx-0">
       <motion.div
         className="flex flex-col items-center justify-center text-center p-10 w-full xl:absolute xl:top-24/5 xl:left-1/7 xl:w-1/4 bg-base-200 rounded-4xl shadow-lg z-10 mb-5 xl:mb-0"
         initial={{ x: 100, opacity: 0 }}
@@ -12,8 +12,11 @@ export default function BrandingHomePage() {
         transition={{ type: "spring", stiffness: 50, damping: 20, duration: 0.8 }}
         >
         <h1 className="text-4xl font-bold mb-4">Branding</h1>
+        <p>
+          Want your logo rolling around town?
+        </p>
         <p className="mb-5">
-          Want your logo rolling around town? Here at Limo Butler Pte Ltd, we can make it happen.
+          Limo Butler Pte Ltd can make it happen.
         </p>
         <Link to="/branding" className="btn btn-primary">Find out more</Link>
       </motion.div>
